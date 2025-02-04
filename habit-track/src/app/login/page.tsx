82f922aeae1d057d7c534e32505f9d877b867f7a
aznshrllh <ahmadzakinashirullah@gmail.com>
2025-02-04 16:28:22 +0700

    feat: add daisyui for enhanced UI components and update login page styles

--- habit-track/src/app/login/page.tsx
+++ habit-track/src/app/login/page.tsx
@@ -26,14 +26,14 @@ export default function LoginPage() {
   return (
     <>
       <ErrorNotification />
-      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="flex items-center justify-center min-h-screen bg-gray-200">
         <div className="w-full max-w-md p-8 space-y-6 bg-white rounded shadow-md">
-          <h2 className="text-2xl font-bold text-center">Login</h2>
+          <h2 className="text-2xl font-bold text-center">LOGIN</h2>
           <form onSubmit={onSubmit} className="space-y-4">
             <div>
               <label
                 htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
+                className="block text-sm font-bold text-gray-700"
               >
                 Email
               </label>
@@ -44,14 +44,14 @@ export default function LoginPage() {
                 placeholder="Enter your email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                className="w-full px-3 py-2 mt-1 border rounded-md focus:outline-none focus:ring focus:ring-indigo-200"
+                className="w-full px-3 py-2 mt-1 border rounded-md focus:outline-none focus:ring focus:ring-blue-200"
                 required
               />
             </div>
             <div>
               <label
                 htmlFor="password"
-                className="block text-sm font-medium text-gray-700"
+                className="block text-sm font-bold text-gray-700"
               >
                 Password
               </label>
@@ -62,13 +62,13 @@ export default function LoginPage() {
                 placeholder="Enter your password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                className="w-full px-3 py-2 mt-1 border rounded-md focus:outline-none focus:ring focus:ring-indigo-200"
+                className="w-full px-3 py-2 mt-1 border rounded-md focus:outline-none focus:ring focus:ring-blue-200"
                 required
               />
             </div>
             <button
               type="submit"
-              className="w-full px-4 py-2 font-bold text-white bg-indigo-600 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-200"
+              className="btn w-full rounded-full bg-blue-300"
             >
               Login
             </button>

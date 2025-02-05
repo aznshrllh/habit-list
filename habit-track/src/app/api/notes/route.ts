86149270fd0184fe75740b66eeec6476468a4454
aznshrllh <ahmadzakinashirullah@gmail.com>
2025-02-05 20:10:37 +0700

    feat: implement error handling in POST and GET routes for notes with user validation

--- habit-track/src/app/api/notes/route.ts
+++ habit-track/src/app/api/notes/route.ts
@@ -1,53 +1,54 @@
 import NoteModel from "@/db/models/noteModel";
 import UserModel from "@/db/models/userModel";
+import errorHandler from "@/helpers/errorHandler";
+import { AppError } from "@/types";
 
 export async function POST(request: Request) {
-  const userId = request.headers.get("x-user-id") as string;
-  // console.log(userId, "<<<< userId");
-  const body = await request.json();
-  // console.log(body, "<<<< body");
+  try {
+    const userId = request.headers.get("x-user-id") as string;
+    const body = await request.json();
 
-  const user = await UserModel.findById(userId);
+    const user = await UserModel.findById(userId);
 
-  if (!user) {
-    return new Response(JSON.stringify({ message: "User not found" }), {
-      status: 404,
+    if (!user) {
+      throw { message: "User not found", status: 404 };
+    }
+
+    if (!body.name || !body.description) {
+      throw { message: "Name and description are required", status: 400 };
+    }
+
+    const newNote = {
+      ...body,
+      userId: userId,
+    };
+
+    // console.log(newNote, "<<<< newNote");
+
+    const note = await NoteModel.create(newNote);
+    return new Response(JSON.stringify({ message: `success adding notes` }), {
+      status: 201,
     });
+  } catch (error) {
+    return errorHandler(error as AppError);
   }
-
-  if (!body.name || !body.description) {
-    return new Response(
-      JSON.stringify({ message: "Name and description are required" }),
-      { status: 400 }
-    );
-  }
-
-  const newNote = {
-    ...body,
-    userId: userId,
-  };
-
-  // console.log(newNote, "<<<< newNote");
-
-  const note = await NoteModel.create(newNote);
-  return new Response(JSON.stringify({ message: `success adding notes` }), {
-    status: 201,
-  });
 }
 
 export async function GET(request: Request) {
-  const userId = request.headers.get("x-user-id") as string;
-  // console.log(userId, "<<<< userId");
+  try {
+    const userId = request.headers.get("x-user-id") as string;
+    // console.log(userId, "<<<< userId");
 
-  const user = await UserModel.findById(userId);
+    const user = await UserModel.findById(userId);
 
-  if (!user) {
-    return new Response(JSON.stringify({ message: "User not found" }), {
-      status: 404,
-    });
+    if (!user) {
+      throw { message: "User not found", status: 404 };
+    }
+
+    const notes = await NoteModel.findByUserId(userId);
+
+    return new Response(JSON.stringify(notes), { status: 200 });
+  } catch (error) {
+    return errorHandler(error as AppError);
   }
-
-  const notes = await NoteModel.findByUserId(userId);
-
-  return new Response(JSON.stringify(notes), { status: 200 });
 }
